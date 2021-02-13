96caba3df93ac5b7f7677bb15845e064abc951d3
ColinKYuen <ColinKYuen@users.noreply.github.com>
2021-02-13 12:38:16 -0800

    Initial Commit
    Includes Assets & OnClick Script

---
+++ Assets/Scripts/ObjectClicker.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectClicker : MonoBehaviour
+{
+    // Object To Spawn
+    public GameObject objToSpawn;
+
+    private void Update()
+    {
+
+    }
+
+    void OnMouseDown()
+    {
+        Debug.Log("Sprite Clicked");
+        spawnObject();
+    }
+
+    public void spawnObject()
+    {
+        // Spawn Location
+        Vector3 clickPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        clickPosition.z = 0;
+        // Spawn Object
+        Instantiate(objToSpawn, clickPosition, Quaternion.identity);
+    }
+}
